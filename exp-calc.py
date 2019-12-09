977f78168a114b2c249667b920da03363295cc63
evanmce <ecm53@pitt.edu>
2019-12-09 02:05:42 -0500

    added fast and med fast lvl calc

--- exp-calc.py
+++ exp-calc.py
@@ -1,4 +1,23 @@
 import argparse
+import math
+
+def calc_fast_lvls():
+    lvls = []
+    xp = 0
+    for i in range(1, 101):
+        xp = math.floor(4*(i**3)/5)
+        lvls.append(xp)
+    return lvls
+
+def calc_med_fast_lvls():
+    lvls = []
+    xp = 0
+    for i in range(1, 101):
+        xp = i**3
+        lvls.append(xp)
+    lvls[0] = 0
+    return lvls
+
 
 def main():
     parser = argparse.ArgumentParser()
@@ -13,6 +32,12 @@ def main():
 
     if args.candy:
         print("calculating using XS candies")
+
+    fast = calc_fast_lvls()
+    med_fast = calc_med_fast_lvls()
+
+    for x in range(len(med_fast)):
+        print(med_fast[x])
 
 if __name__ == "__main__":
     main()
